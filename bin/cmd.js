7de10cb8dc96a8489d5f7d9a57719a1eb24f4c94
ruanyf <yifeng.ruan@gmail.com>
2016-07-13 11:56:15 +0800

    feat: first commit

---
+++ bin/cmd.js
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+'use strict';
+
+const path = require('path');
+
+const argv = require('yargs')
+  .usage('Usage: $0 -w [num] -h [num]')
+  .option('dir', {
+    alias: 'd',
+    default: './docs',
+    describe: 'document directory',
+    type: 'string'
+  })
+  .option('output', {
+    alias: 'o',
+    default: './dist',
+    describe: 'output directory',
+    type: 'string'
+  })
+  .option('debug', {
+    default: false,
+    describe: 'debug mode',
+    type: 'boolean'
+  })
+  .option('version', {
+    alias: 'v',
+    describe: 'version information',
+    type: 'boolean'
+  })
+  .option('help', {
+    alias: 'h',
+    describe: 'help information',
+    type: 'boolean'
+  })
+  .help('help')
+  .epilog('copyright 2016')
+  .argv;
+
+if (argv.version) {
+  let pkg = require(path.join(__dirname, '../package.json'));
+  console.log(pkg.version);
+  process.exit(0);
+}
+
+if (argv.debug) {
+  process.env['DEBUG'] = '*';
+}
+
+require('../lib')(argv);
+
