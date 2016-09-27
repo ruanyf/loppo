f54e99357bb878e2bff179ac7885898a7cfb7999
ruanyf <yifeng.ruan@gmail.com>
2016-09-27 08:31:20 +0800

    feat: add theme.js's test cases

--- test/theme.test.js
+++ test/theme.test.js
@@ -2,6 +2,7 @@
 
 const path = require('path');
 const test = require('tape');
+const fs = require('fs-extra');
 const theme = require('../lib/theme');
 // const walkSync = require('walk-sync');
 
@@ -19,8 +20,22 @@ test(
     process.chdir(TEST_PATH);
     const option = theme({ theme: THEME });
     // const result = walkSync(path.resolve(process.cwd(), 'themes', THEME));
-
     t.equal(option.templates.page(), 'hello world\n');
     t.end();
+  }
+);
+
+test(
+  test_title +
+  'if the theme directory does not exist, the directory will be created',
+  function (t) {
+    const TEST_PATH = path.resolve(__dirname, './fixture/theme/theme-not-exists');
+    process.chdir(TEST_PATH);
+    const option = theme({ theme: THEME });
+    // const result = walkSync(path.resolve(process.cwd(), 'themes', THEME));
+
+    t.equal(typeof option.templates.page, 'function');
+    t.end();
+    fs.removeSync(path.resolve(process.cwd(), 'themes', THEME));
   }
 );
