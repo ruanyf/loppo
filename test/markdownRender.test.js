df2d959ae6c67addccef631937c73f49be48c2a5
ruanyf <yifeng.ruan@gmail.com>
2016-12-08 22:33:16 +0800

    refactor: update dependencies version

--- test/markdownRender.test.js
+++ test/markdownRender.test.js
@@ -8,7 +8,7 @@ const markdownRender = require('../lib/utils').markdownRender;
 const log = require('../lib/utils').log;
 log.setLevel('error');
 
-const test_title = '[utils.js/makedownRender] ';
+const test_title = '[utils.js/markdownRender] ';
 
 test(
   test_title +
@@ -27,7 +27,7 @@ test(
     };
     const optionObj = markdownRender('dir1/a.md', opt);
     t.equal(optionObj.toc, '');
-    t.equal(optionObj.content, '<h1 id="my-title"><a class="markdownIt-Anchor" href="#my-title">#</a> My Title</h1>\n<p>This is an example.</p>\n');
+    t.equal(optionObj.content, '<h1 id="my-title">My Title <a class="markdownIt-Anchor" href="#my-title">#</a></h1>\n<p>This is an example.</p>\n');
     t.end();
   }
 );
@@ -49,7 +49,7 @@ test(
     };
     const optionObj = markdownRender('dir1/a.md', opt);
     t.equal(optionObj.toc, '<ul class="markdownIt-TOC">\n<li><a href="#html-%E6%96%87%E6%A1%A3%E7%9A%84%E6%A0%87%E9%A2%98">HTML 文档的”标题“</a></li>\n</ul>\n');
-    t.equal(optionObj.content, '<h2 id="html-文档的标题"><a class="markdownIt-Anchor" href="#html-文档的标题">#</a> HTML 文档的”标题“</h2>\n');
+    t.equal(optionObj.content, '<h2 id="html-文档的标题">HTML 文档的”标题“ <a class="markdownIt-Anchor" href="#html-文档的标题">#</a></h2>\n');
     t.end();
   }
 );
@@ -71,7 +71,7 @@ test(
     };
     const optionObj = markdownRender('dir1/a.md', opt);
     t.equal(optionObj.toc, '<ul class="markdownIt-TOC">\n<li><a href="#title-1">Title 1</a></li>\n<li><a href="#title-2">Title 2</a>\n<ul>\n<li><a href="#title-3">Title 3</a></li>\n<li><a href="#title-4">Title 4</a></li>\n</ul>\n</li>\n</ul>\n');
-    t.equal(optionObj.content, '<h1 id="my-title"><a class="markdownIt-Anchor" href="#my-title">#</a> My Title</h1>\n<h2 id="title-1"><a class="markdownIt-Anchor" href="#title-1">#</a> Title 1</h2>\n<h2 id="title-2"><a class="markdownIt-Anchor" href="#title-2">#</a> Title 2</h2>\n<h3 id="title-3"><a class="markdownIt-Anchor" href="#title-3">#</a> Title 3</h3>\n<h3 id="title-4"><a class="markdownIt-Anchor" href="#title-4">#</a> Title 4</h3>\n');
+    t.equal(optionObj.content, '<h1 id="my-title">My Title <a class="markdownIt-Anchor" href="#my-title">#</a></h1>\n<h2 id="title-1">Title 1 <a class="markdownIt-Anchor" href="#title-1">#</a></h2>\n<h2 id="title-2">Title 2 <a class="markdownIt-Anchor" href="#title-2">#</a></h2>\n<h3 id="title-3">Title 3 <a class="markdownIt-Anchor" href="#title-3">#</a></h3>\n<h3 id="title-4">Title 4 <a class="markdownIt-Anchor" href="#title-4">#</a></h3>\n');
     t.end();
   }
 );
@@ -93,7 +93,7 @@ test(
     };
     const optionObj = markdownRender('dir1/a.md', opt);
     t.equal(optionObj.toc, '<ul class="markdownIt-TOC">\n<li><a href="#%E4%BD%A0%E5%A5%BD">你好</a>\n<ul>\n<li><a href="#%E4%B8%96%E7%95%8C">世界</a></li>\n</ul>\n</li>\n</ul>\n');
-    t.equal(optionObj.content, '<h2 id="你好"><a class="markdownIt-Anchor" href="#你好">#</a> 你好</h2>\n<h3 id="世界"><a class="markdownIt-Anchor" href="#世界">#</a> 世界</h3>\n');
+    t.equal(optionObj.content, '<h2 id="你好">你好 <a class="markdownIt-Anchor" href="#你好">#</a></h2>\n<h3 id="世界">世界 <a class="markdownIt-Anchor" href="#世界">#</a></h3>\n');
     t.end();
   }
 );
